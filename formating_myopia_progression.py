4d64264cd99a90aa46142a1f72574dbbfaa126da
Yunlong Ma <11418032@zju.edu.cn>
2020-09-03 13:48:07 +0800

    Update formating_myopia_progression.py

--- formating_myopia_progression.py
+++ formating_myopia_progression.py
@@ -9,47 +9,56 @@ Created on Wed Sep  2 16:28:27 2020
 #E-mail: glb-biotech@zju.edu.cn
 
 #This code was used for formating the data for Generalized mixed linear regression model on myopia progression
+f3 = open("2.1.model1-common.txt", "r+")
+f4 = open("data_for_myopia_progression.txt","w+")
 
-#----------------Start------------------------------------
+import time #used for monitoring the time of script operating
 
-f1 = open("2.1.model1-common.txt", "r+")
-
-f2 = open("data_for_myopia_progression.txt","w+")
+#test example
+#f3 = open("test_model1.txt", "r+")
+#f3_1 = open("test_model1.txt", "r+")
+#f4 = open("data_for_onset_myopia_test663.txt","w+")
 
 
 #collect samples ID for excluding from further analysis
-ID_collect=[]
-lines=""
-for line in f1:
+start =time.clock()
+ID_collect={}
+#lines=""
+for line in f3:
     dd = line.strip().split("\t")
-    lines=lines+line
+    #lines=lines+line
     if dd[4] != "od_result" and int(dd[4])>3:
-        ID_collect.append(dd[0])
-    if  dd[6]=="0":
-        ID_collect.append(dd[0])
+        ID_collect[dd[0]] = 1
+    #if  dd[6]=="0":
+      #  ID_collect[dd[0]] =1
       
 
 #obtain unique ID    
-remove_id = list(set(ID_collect))
+#remove_id = list(set(ID_collect)) # using list is too waste time
+#remove_id = set(ID_collect)
+     
+end = time.clock()
+print('Running time1: %s Seconds'%(end-start))
+
+
 
 #keep samples we need
 #remove od_SE >4 and its related ID
-
-lines_out=''
-new_data=lines.split("\n")
-for x in new_data:
+#remove individuals with myopia at baseline time point and its related ID
+start =time.clock()
+#lines_out=''
+#new_data=lines.split("\n")
+for x in open("2.1.model1-common.txt", "r+"):
     tt = x.strip().split("\t")
-    if tt[0] not in remove_id:
-        lines_out = lines_out + "\t".join(tt) + "\n"
+    if tt[0] not in ID_collect:
+        #lines_out = lines_out + "\t".join(tt) + "\n" #too waste time
+        f4.write("\t".join(tt) + "\n")
     
     
-f2.write(lines_out)
+#f2.write(lines_out)
+end = time.clock()
+print('Running time3: %s Seconds'%(end-start))
 
-
-
-
-#----------------End------------------------------------
-
-
-
+f3.close()
+f4.close()
 
